86a769344323a5e80dd5bf7966afa7b884038910
Luca Panariello <luca.panariello@studio.unibo.it>
2022-05-06 11:50:14 +0200

    cors added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 using ProvaRest.Models;
 using ProvaRest.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://lucapanariello.altervista.org",
+                                              "https://lucapanariello.altervista.org/comments");
+                      });
+});
 
 // Add services to the container.
 builder.Services.Configure<CommentDatabaseSettings>(
@@ -24,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
