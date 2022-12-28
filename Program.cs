12d0df094f4162fa08058064421e15dbc0cec638
Luca <luca.panariello@gmail.com>
2022-12-28 21:47:22 +0100

    fixed

--- Program.cs
+++ Program.cs
@@ -12,34 +12,16 @@ builder.Services.Configure<CommentDatabaseSettings>(
 builder.Services.AddSingleton<CommentService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-/* Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-} */
-
-// app.UseRouting();
-
 app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Benvenuto nel web service della mia pagina personale main");
 
 app.UseCors(options =>
     options.WithOrigins("https://lucapanariello.altervista.org")
            .WithMethods("POST", "PUT", "DELETE", "GET")
            .AllowAnyHeader());
 
-/*
-app.UseCors(options => 
-    options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-*/
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
