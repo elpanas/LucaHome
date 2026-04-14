a154fd0b779792d64d44f5cc5a7f8c37cd51e69f
Luca Panariello <luca.panariello@gmail.com>
2026-04-14 22:21:37 +0200

    aggiunti decoratori commentDTOin

--- DTO/CommentDTOIn.cs
+++ DTO/CommentDTOIn.cs
@@ -1,9 +1,20 @@
-﻿namespace LucaHome.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LucaHome.DTO
 {
     public class CommentDTOIn
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(500, MinimumLength = 10)]
         public string Content { get; set; } = null!;
+        public string? Middlename { get; set; }
     }
 }
