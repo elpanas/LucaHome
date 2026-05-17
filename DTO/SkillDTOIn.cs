4a8efd671090cf839b9fc90764c65982f293d72a
Luca Panariello <luca.panariello@gmail.com>
2026-05-17 22:05:52 +0200

    aggiunti DTO Sill

---
+++ DTO/SkillDTOIn.cs
@@ -0,0 +1,9 @@
+﻿namespace LucaHome.DTO
+{
+    public class SkillDTOIn
+    {
+        public string? Name { get; set; }
+        public int? Category { get; set; }
+        public int? Sequential { get; set; }
+    }
+}
---
+++ DTO/SkillDTOOut.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LucaHome.DTO
+{
+    public class SkillDTOOut
+    {
+        public string? Id { get; set; }  
+        public string? Name { get; set; }
+        public int? Category { get; set; }    
+        public int? Sequential { get; set; }
+    }
+}
