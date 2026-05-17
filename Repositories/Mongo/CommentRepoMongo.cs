be4db6146c84a3b680776a4cc2524e967f5173ce
Luca Panariello <luca.panariello@gmail.com>
2026-05-17 22:12:32 +0200

    repository divise per database

---
+++ Repositories/Mongo/CommentRepoMongo.cs
@@ -0,0 +1,32 @@
+﻿using LucaHome.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace LucaHome.Repositories.Mongo
+{
+    public class CommentRepoMongo : ICommentRepository
+    {
+        private readonly IMongoCollection<Comment> _commentsCollection;
+
+        public CommentRepoMongo(IOptions<MongoSettings> DbSettings)
+        { 
+            var mongoClient = new MongoClient(
+                DbSettings.Value.ConnectionStringMongo);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                DbSettings.Value.DatabaseName);
+
+            _commentsCollection = mongoDatabase.GetCollection<Comment>(
+                DbSettings.Value.CommentCollectionName); 
+        }
+
+        public async Task<Comment> GetByIdAsync(string id) =>
+             await _commentsCollection.Find(c => c.Id == id).FirstAsync();
+
+        public async Task<List<Comment>> GetAllAsync() =>
+            await _commentsCollection.Find(_ => true).ToListAsync();
+
+        public async Task CreateAsync(Comment comment) =>
+            await _commentsCollection.InsertOneAsync(comment);
+    }
+}
---
+++ Repositories/Mongo/UserRepoMongo.cs
@@ -0,0 +1,30 @@
+﻿using LucaHome.DTO;
+using LucaHome.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace LucaHome.Repositories.Mongo
+{
+    public class UserRepoMongo : IUserRepository
+    {
+        private readonly IMongoCollection<User> _usersCollection;
+
+        public UserRepoMongo(IOptions<MongoSettings> DbSettings)
+        {
+            var mongoClient = new MongoClient(DbSettings.Value.ConnectionStringMongo);
+            var mongoDatabase = mongoClient.GetDatabase(DbSettings.Value.DatabaseName);
+            _usersCollection = mongoDatabase.GetCollection<User>(DbSettings.Value.UserCollectionName);
+        }
+
+        public async Task<User?> GetUserByUsername(string username)
+        {
+            // Cerco l'utente per username
+            var query = from u in _usersCollection.AsQueryable()
+                        where u.Username == username
+                        select u;
+
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
