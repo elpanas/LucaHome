267d707e1d52f7f108c5f1c29cf75338b0eb255f
Luca Panariello <luca.panariello@gmail.com>
2026-05-17 22:05:38 +0200

    aggiunto context SQL

---
+++ DBs/SQLDBContext.cs
@@ -0,0 +1,33 @@
+﻿using LucaHome.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LucaHome.DBs
+{
+    public class SQLDBContext : DbContext
+    {
+        public SQLDBContext(DbContextOptions<SQLDBContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(table => { 
+                table.HasKey(u => u.Id);
+                table.Property(u => u.Username)
+                        .HasMaxLength(50)
+                        .IsRequired();  
+                table.Property(u => u.Password)
+                        .HasMaxLength(255)
+                        .IsRequired();
+            });
+
+            modelBuilder.Entity<Skill>(table => table.HasKey(s => s.Id));
+
+            modelBuilder.Entity<Comment>(table => table.HasKey(c => c.Id));
+        }
+
+    }
+}
