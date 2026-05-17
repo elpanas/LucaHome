8ce97e400547dc8d0c2bb60c18ac0262644169e5
Luca Panariello <luca.panariello@gmail.com>
2026-05-17 22:06:43 +0200

    aggiunto skill mapper

---
+++ Mappers/SkillMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LucaHome.DTO;
+using LucaHome.Models;
+
+namespace LucaHome.Mappers
+{
+    public class SkillMapper : Profile
+    {
+        public SkillMapper()
+        {
+            CreateMap<SkillDTOIn, Skill>();
+            CreateMap<Skill, SkillDTOOut>();
+        }
+    }
+}
