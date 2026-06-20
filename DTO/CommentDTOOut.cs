3d51669a974e831b856febc4b108b81aca26b83f
Luca Panariello <luca.panariello@gmail.com>
2026-06-20 17:40:08 +0200

    aggiunto campo data

--- DTO/CommentDTOOut.cs
+++ DTO/CommentDTOOut.cs
@@ -2,9 +2,10 @@
 {
     public class CommentDTOOut
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Content { get; set; } = null!;
+        public DateTime Datetime { get; set; }
     }
 }
