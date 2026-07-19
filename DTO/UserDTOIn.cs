f31ca08b697051842e276a123699a7692face257
Luca Panariello <luca.panariello@gmail.com>
2026-07-19 18:05:08 +0200

    aggiunto campo Signature al DTOIn

--- DTO/UserDTOIn.cs
+++ DTO/UserDTOIn.cs
@@ -5,5 +5,6 @@
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? Ciphertext { get; set; } // Ciphertext PQC
+        public string? Signature { get; set; } // Signature PQC
     }
 }
